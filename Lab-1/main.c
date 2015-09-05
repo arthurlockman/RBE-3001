090e215c54132e90806b4081fec29a66b6c5ee18
Tucker Haydon <tchaydon@wpi.edu>
2015-09-05 19:40:20 +0000

    Fuck magic registers
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@24 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -11,14 +11,19 @@ int main(void)
 {
 	debugUSARTInit(DEFAULT_BAUD);
 	initRBELib();
-	setPinsDir('A', INPUT, 1, PORT7);
+
+	setPinsDir('A', OUTPUT, 2, PORT3, PORT5);
+	setPinsVal('A', LOW, 1, PORT3);
+	setPinsVal('A', HIGH, 1, PORT5);
+
 	initADC(7, ADC_FREE_RUNNING, ADC_REF_VCC);
 	while (1)
 	{
 		printf("Reading... ");
-		long val = getADC(7);
-		printf("Value: %ld\n\r", val);
+	    long val = getADC(7);
+		printf("Value1: %d\n\r", ADCSRA);
 		_delay_ms(1000);
 	}
+
 	return 0;
 }
