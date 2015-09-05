ff835705179d7171fd6cb4c8ddd07a1efa0966fd
Tucker Haydon <tchaydon@wpi.edu>
2015-09-05 20:50:18 +0000

    Created readADCValues function
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@28 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -7,23 +7,23 @@
 #include "RBELib/RBELib.h"
 #include "RBELib/ADC.h"
 
-int main(void)
+void readADCValues()
 {
 	debugUSARTInit(DEFAULT_BAUD);
 	initRBELib();
 
-	setPinsDir('A', OUTPUT, 2, PORT3, PORT5);
-	setPinsVal('A', LOW, 1, PORT3);
-	setPinsVal('A', HIGH, 1, PORT5);
-
 	initADC(7, ADC_FREE_RUNNING, ADC_REF_VCC);
 	while (1)
 	{
-		printf("Reading... ");
 	    long val = getADC(7);
 		printf("%ld\n\r", val);
 		_delay_ms(10);
 	}
+}
 
+int main(void)
+{
+	readADCValues();
 	return 0;
 }
+
