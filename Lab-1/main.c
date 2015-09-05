96a679adaface53ae641ef3591dce8e41dbac00b
Arthur Lockman <hello@rthr.me>
2015-09-05 18:28:23 +0000

    Updated main functions.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@23 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -9,9 +9,16 @@
 
 int main(void)
 {
-	initADC(7, ADC_FREE_RUNNING, ADC_VREF_VCC);
+	debugUSARTInit(DEFAULT_BAUD);
+	initRBELib();
+	setPinsDir('A', INPUT, 1, PORT7);
+	initADC(7, ADC_FREE_RUNNING, ADC_REF_VCC);
 	while (1)
 	{
+		printf("Reading... ");
+		long val = getADC(7);
+		printf("Value: %ld\n\r", val);
+		_delay_ms(1000);
 	}
 	return 0;
 }
