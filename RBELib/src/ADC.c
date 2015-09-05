c5b5542c67ac7e3f11f5926febe1a07ac990f447
Tucker Haydon <tchaydon@wpi.edu>
2015-09-05 20:44:03 +0000

    Made the ADC work.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@26 015edbb1-928d-476b-afdc-d2170ecc5f68

--- RBELib/src/ADC.c
+++ RBELib/src/ADC.c
@@ -7,21 +7,11 @@
 #include <RBELib/RBELib.h>
 #include <RBELib/ADC.h>
 
-/**
- * @brief Initializes the ADC and make one channel active.
- * You can choose to use either interrupts or polling to read
- * the desired channel.
- *
- * @param channel The ADC channel to initialize.
- *
- * @todo Create the corresponding function to initialize the ADC
- * using the channel parameter.
- */
 void initADC(int channel, adcMode mode, adcVref vref)
 {
 	// For these bits, see page 259 of the guide
 	// Enable the ADC, allow interrupts, and set the clock division
-	ADCSRA = BIT(ADEN) | (BIT(ADSC) & 0) | (BIT(ADATE) & 0) | (BIT(ADIF) & 0) | BIT(ADIE) | BIT(ADPS2) | BIT(ADPS1) | BIT(ADPS0);
+	ADCSRA = BIT(ADEN) | (BIT(ADSC) & 0) | BIT(ADATE) | (BIT(ADIF) & 0) | (BIT(ADIE) & 0) | BIT(ADPS2) | BIT(ADPS1) | BIT(ADPS0);
 
 	// For these bits, see page 262 of the guide
 	// Set the trigger mode for the ADC
@@ -37,14 +27,6 @@ void initADC(int channel, adcMode mode, adcVref vref)
 	changeADC(channel);
 }
 
-/**
- * @brief Disables ADC functionality and clears any saved values (globals).
- *
- * @param channel  The ADC channel to disable.
- *
- * @todo Create the corresponding function to clear the last ADC
- * calculation register and disconnect the input to the ADC if desired.
- */
 void clearADC(int channel)
 {
 	// For these bits, see page 259 of the guide
@@ -58,52 +40,20 @@ void clearADC(int channel)
 	changeADC(-1);
 }
 
-/**
- * @brief Run a conversion on and get the analog value from one ADC
- * channel if using polling.
- *
- * @param channel  The ADC channel to run a conversion on.
- * @return adcVal The 8-10 bit value returned by the ADC
- * conversion.  The precision depends on your settings and
- * how much accuracy you desire.
- *
- * @todo Create the corresponding function to obtain the value of the
- * last calculation if you are using polling.
- */
 UINT16 getADC(int channel)
 {
-
-	//changeADC(channel);
-//	ADCSRA &= !BIT(ADATE);
-//	ADCSRA |= BIT(ADSC);
-//	while (ADCSRA & BIT(ADSC))
-//		;
-//	return getADCValue();
-	return 1;
+	changeADC(channel);
+	ADCSRA |= BIT(ADSC);
+	while ((ADCSRA & BIT(ADIF)) == 0)
+		;
+	return getADCValue();
 }
 
 UINT16 getADCValue()
 {
-	if (ADMUX & BIT(ADLAR)) //Left adjusted
-	{
-		UINT16 val = ADCH << 2;
-		val |= (ADCL & 0b11000000) >> 6;
-		return val;
-	}
-	else //right adjusted
-	{
-		UINT16 val = (ADCH & 0b00000011) << 8;
-		val |= ADCL;
-		return val;
-	}
+	return ADCW;
 }
-/**
- * @brief Change the channel the ADC is sampling if using interrupts.
- *
- * @param channel  The ADC channel to switch to.
- *
- * @todo Create a way to switch ADC channels if you are using interrupts.
- */
+
 void changeADC(int channel)
 {
 	switch (channel)
