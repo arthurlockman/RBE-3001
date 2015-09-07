30f97d6b6547dc8a188ca327018b1818146955ec
rmputnam <rmputnam@wpi.edu>
2015-09-07 23:35:50 +0000

    Finished the waveform function
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@36 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -7,11 +7,17 @@
 #include "RBELib/RBELib.h"
 #include "RBELib/ADC.h"
 
-volatile unsigned long count = 0;
+volatile unsigned int count = 0;
+volatile unsigned long ms = 0;
 
-ISR(TIMER1_COMPA_vect)
+ISR(TIMER0_OVF_vect)
 {
 	count++;
+	if (count == 9)
+	{
+		ms++;
+		count = 0;
+	}
 }
 
 void readADCValues()
@@ -33,11 +39,56 @@ void readADCValues()
 	}
 }
 
+void outputWave()
+{
+	debugUSARTInit(DEFAULT_BAUD);
+	initRBELib();
+	//TODO: Move to function
+	TCCR0A = 0b00000000;
+	TCCR0B = 0b00000010;
+	TIMSK0 = 0b00000001;
+
+	unsigned long start;
+	double duty;
+	int period = 100;
+
+    initADC(7, ADC_FREE_RUNNING, ADC_REF_VCC);
+
+	setPinsDir('C', OUTPUT, 1, PORT6);
+	setPinsDir('B', INPUT, 3, PORT0, PORT1, PORT2);
+	while (1)
+	{
+		unsigned long potValue = getADC(7);
+		duty = potValue / 1023.0;
+		start = ms;
+		while (ms - start < period * duty)
+		{
+			setPinsVal('C', HIGH, 1, PORT6);
+		}
+
+		start = ms;
+		while (ms - start < period * (1 - duty))
+		{
+		    setPinsVal('C', LOW, 1, PORT6);
+		}
+
+		//B0, B1, B2
+		if (getPinsVal('B',1,PORT0) == 0b00000001) {
+			period = 1000;
+		} else if (getPinsVal('B',1,PORT1) == 0b00000010) {
+			period = 50;
+		} else if (getPinsVal('B',1,PORT2) == 0b00000100) {
+			period = 10;
+		}
+		double frequency = 1000.0 / period;
+		printf("Duty Cycle: %f\tFrequency: %f\tState: \tPot Value: %ld\n\r", duty, frequency, potValue);
+	}
+}
+
 int main(void)
 {
 	sei();
-	initTimer(1, CTC, 0xFF);
-	readADCValues();
+	outputWave();
 	return 0;
 }
 
