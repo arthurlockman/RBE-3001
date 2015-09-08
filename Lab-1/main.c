7ab9e4e4910e1c9601c320a489835a2b2251b796
Tucker Haydon <tchaydon@wpi.edu>
2015-09-08 02:46:21 +0000

    Added lab code.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@40 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-1/main.c
+++ Lab-1/main.c
@@ -5,18 +5,26 @@
  *      Author: ewillcox
  */
 #include "RBELib/RBELib.h"
-#include "RBELib/ADC.h"
 
-volatile unsigned int count = 0;
+volatile char readADCFlag = 0;
 volatile unsigned long ms = 0;
+volatile unsigned long count = 0;
+volatile unsigned tenthMS = 0;
+
 
 ISR(TIMER0_OVF_vect)
 {
 	count++;
+	tenthMS++;
 	if (count == 9)
 	{
 		ms++;
-		count = 0;
+		count = 1;
+	}
+	if(tenthMS == 40) // 10000 / hz;
+	{
+		readADCFlag = 1;	// Resets the flag every 4 ms - 225 Hz
+		tenthMS = 0;
 	}
 }
 
@@ -24,26 +32,77 @@ void readADCValues()
 {
 	debugUSARTInit(DEFAULT_BAUD);
 	initRBELib();
-
+	sei();
+	TCCR0A = 0b00000000;
+	TCCR0B = 0b00000010;
+	TIMSK0 = 0b00000001;
 	initADC(7, ADC_FREE_RUNNING, ADC_REF_VCC);
-	int counter = 0;
 	while (1)
 	{
 	    long val = getADC(7);
 	    double potVal = (5000.0 * val) / 1024.0;
 	    double potAngle = (300.0 * val) / 1024.0;
-	    double timestamp = ((double)counter * 10);
-		printf("%f, %ld, %f, %f\n\r", timestamp, val, potVal, potAngle);
-		_delay_ms(10);
-		counter++;
+		printf("%ld, %ld, %f, %f\n\r", ms, val, potVal, potAngle);
+		long start = ms;
+		while(ms - start < 10)
+			;
 	}
+}
+
+
+void readADCValues2()
+{
+	debugUSARTInit(DEFAULT_BAUD);
+	initRBELib();
+	initADC(6, ADC_FREE_RUNNING, ADC_REF_VCC);
+
+	setPinsDir('B', INPUT, 1, PORT4);				// Set B4 to input
+	setPinsVal('B', 1, 1, PORT4);
+
+	sei();											// Set global interrupt enable
+	// Begin the timer.
+	TCCR0A = 0b00000000;
+	TCCR0B = 0b00000010;
+	TIMSK0 = 0b00000001;
+
+	long buff[225];
+	int i = 0;
+	while(1)
+	{
+		if(getPinsVal('B',1,PORT4) == 0b00010000)	// If the button is pressed
+		{
+			printf("Button has been pressed.\n\r");
+			unsigned long start = ms;
+			while(ms - start <= 1000)				// Scan inputs for 1 second
+			{
+				if(readADCFlag == 1)				// Read the ADC at 225 Hz
+				{
+					readADCFlag = 0;
+					long val = getADC(6);
+					// double potVal = (5000.0 * val) / 1023.0;
+					// double potAngle = (300.0 * val) / 1023.0;
+					// long timestamp = ms;
+					// printf("%ld, %ld, %f, %f\n\r", timestamp, val, potVal, potAngle);
+					buff[i] = val;
+					i++;
+				}
+			}
+			break;
+		}
+	}
+	int p;
+	for(p = 0; p < 225; p++)
+	{
+		printf("%ld,\t", buff[p]);
+	}
+
 }
 
 void outputWave()
 {
 	debugUSARTInit(DEFAULT_BAUD);
 	initRBELib();
-	//TODO: Move to function
+	sei();
 	TCCR0A = 0b00000000;
 	TCCR0B = 0b00000010;
 	TIMSK0 = 0b00000001;
@@ -87,8 +146,9 @@ void outputWave()
 
 int main(void)
 {
-	sei();
-	outputWave();
+	readADCValues();
+//	readADCValues2();
+//	outputWave();
 	return 0;
 }
 
