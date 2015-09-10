1c158d3f24087f26583341d71330e8dbe9b19740
Arthur Lockman <hello@rthr.me>
2015-09-10 15:50:57 +0000

    Added lab 1 part 4.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@46 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-2/main.c
+++ Lab-2/main.c
@@ -5,16 +5,21 @@
  *      Author: ewillcox
  */
 #include "RBELib/RBELib.h"
+#include "RBELib/pot.h"
+
 int main(void)
 {
-	DDRBbits._P4 = OUTPUT;
-	DDRBbits._P4 = OUTPUT;
+	debugUSARTInit(DEFAULT_BAUD);
+	initRBELib();
+	initADC(3, ADC_FREE_RUNNING, ADC_REF_VCC);
+	initPot(DEFAULT_POT_CALIBRATION);
+
 	while (1)
 	{
-		PINBbits._P4 = 0;
-		_delay_ms(100);
-		PINBbits._P4 = 1;
-		_delay_ms(100);
+		long adcReading = getADC(3);
+		printf("Raw: %ld, Angle: %d, mV: %d\n\r", adcReading, potAngle(adcReading), potVolts(adcReading));
+		_delay_ms(10);
 	}
+
 	return 0;
 }
