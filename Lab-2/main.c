25ad1461d0b12e594aa36a7b7a6301e224e1a133
Arthur Lockman <hello@rthr.me>
2015-09-10 22:35:55 +0000

    Added ISR.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@51 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-2/main.c
+++ Lab-2/main.c
@@ -1,6 +1,18 @@
 #include "RBELib/RBELib.h"
 #include "RBELib/pot.h"
 
+long ms;
+short count;
+ISR(TIMER0_OVF_vect)
+{
+	count++;
+	if (count == 9)
+	{
+		ms++;
+		count = 1;
+	}
+}
+
 int main(void)
 {
 	debugUSARTInit(DEFAULT_BAUD);
