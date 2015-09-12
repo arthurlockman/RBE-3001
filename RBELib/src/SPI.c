bea6be3ab2ae93955d10db23d0f8ec862e61d33c
Tucker Haydon <tchaydon@wpi.edu>
2015-09-12 16:32:11 +0000

    Created SPI.c
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@53 015edbb1-928d-476b-afdc-d2170ecc5f68

---
+++ RBELib/src/SPI.c
@@ -0,0 +1,19 @@
+/*
+ * SPI.c
+ *
+ *  Created on: Sep 12, 2015
+ *      Author: Tucker Haydon
+ */
+
+#include "RBELib/RBELib.h"
+#include "SPI.h"
+
+void initSPI()
+{
+
+}
+
+unsigned char spiTransceive(BYTE data)
+{
+
+}
