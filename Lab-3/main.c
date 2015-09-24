0bec338363300bb6581db3d5a50bd40474c8e5e3
Tucker Haydon <tchaydon@wpi.edu>
2015-09-24 14:34:02 +0000

    Added Setup stuff
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@92 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-3/main.c
+++ Lab-3/main.c
@@ -5,6 +5,45 @@
  *      Author: ewillcox
  */
 #include "RBELib/RBELib.h"
+
+unsigned long ms;
+short count;
+short pidCount;
+volatile int upperLinkActual, lowerLinkActual;
+volatile long pidOutputUpper, pidOutputLower;
+
+ISR(TIMER0_OVF_vect)
+{
+	count++;
+	if (count == 9)
+	{
+		ms++;
+		count = 1;
+		pidCount++;
+	}
+	if (pidCount == 9)
+	{
+		pidCount = 1;
+		pidOutputUpper = calcPID('U', upperLinkActual);
+		pidOutputLower = calcPID('L', lowerLinkActual);
+	}
+}
+
+void setup()
+{
+	debugUSARTInit(DEFAULT_BAUD);					// Setup serial
+	initRBELib();									// Setup RBELib
+	initSPI();										// Setup SPI/DAC
+	initADC(3, ADC_FREE_RUNNING, ADC_REF_VCC);		// Setup ADC
+	configureMsTimer();								// Setup ms timer and interrupts
+	potCalibration calUpper = { 250, 625, 975 };	// Setup upper arm pot
+	initPot(0, 3, calUpper);
+	potCalibration calLower = { 255, 668, 1100 };	// Setup lower arm pot
+	initPot(1, 2, calLower);
+	setConst('U', 300.0, 0.0, 10.0); 				// Setup upper arm gains
+	setConst('L', 300.0, 0.0, 10.0);				// Setup lower arm gains
+}
+
 int main(void)
 {
 	DDRBbits._P4 = OUTPUT;
