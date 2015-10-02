2b0749a8426bf1877865b2e16f8e757ac8560a19
Tucker Haydon <tchaydon@wpi.edu>
2015-10-02 15:43:34 +0000

    Robot arm works approximately.
    git-svn-id: https://fusion.wpi.edu/svn/rbe3001a15team2/trunk@105 015edbb1-928d-476b-afdc-d2170ecc5f68

--- Lab-4/main.c
+++ Lab-4/main.c
@@ -72,38 +72,108 @@ void home()
 	resetEncCount(1);
 }
 
+int normalVariance(int* readings, int numReadings, float average)
+{
+	float variance = 0;
+	for(int i = 0; i < numReadings; i++)
+	{
+		variance += pow(readings[i] - average, 2);
+	}
+	variance /= numReadings;
+
+	float stdDev = sqrt(variance);
+
+	if(stdDev <= 1) return 1;
+	else return 0;
+
+
+}
+
+int stableValue(int channel)
+{
+	int numReadings = 7;
+	int readings[numReadings];
+	float sum = 0;
+	for(int i = 0; i < numReadings; i++)
+	{
+		readings[i] = IRDist(channel);
+		sum += readings[i];
+		_delay_ms(1);
+	}
+
+	float average = sum / numReadings;
+
+	if((abs(readings[0] - readings[numReadings - 1]) <= 2) && normalVariance(readings, numReadings, average))
+	{
+		return (int)average;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int average(int channel)
+{
+	int numReadings = 7;
+	int readings[numReadings];
+	float sum = 0;
+	for(int i = 0; i < numReadings; i++)
+	{
+		readings[i] = IRDist(channel);
+		sum += readings[i];
+		_delay_ms(1);
+	}
+
+	float average = sum / numReadings;
+	return (int)(average);
+}
+
 pickupInfo calcPickupInfo()
 {
-	// Once we reach the threshhol (12 or 13) AND the value is stable (5-6 readings of the same value), log that time
+	// Once we reach the threshhold (12 or 13) AND the value is stable (5-6 readings of the same value), log that time
 
 	float deltaX = 16;
 	int deltaT;
 	float distanceToArmCenter = 3;
 
-	long startTime = ms;
-
 	int startIRVal_1 = IRDist(6);
 	int startIRVal_2 = IRDist(7);
 
-	printf("Start values: %d, %d\n\r", startIRVal_1, startIRVal_2);
-
-	while(startIRVal_1 - IRDist(6) < 2)
-		printf("Current IR1: %d\n\r", IRDist(6));
+	while(1)
+	{
+		startIRVal_1 = stableValue(6);
+		if(startIRVal_1 != -1 &&  average(6) < 12)
+		{
+			break;
+		}
+	}
 	long passTime1 = ms;
 
-	while(startIRVal_2 - IRDist(7) < 2)
-		printf("Current IR2: %d\n\r", IRDist(7));
+	while(1)
+	{
+		startIRVal_2 = stableValue(7);
+		if(startIRVal_2 != -1 &&  average(7) < 12)
+		{
+			break;
+		}
+	}
 	long passTime2 = ms;
 
+	printf("Start values: %d, %d\n\r", startIRVal_1, startIRVal_2);
+
 	deltaT = (int)(passTime2 - passTime1);
+	printf("Delta time: %d\n\r", deltaT);
 
 	float velocity = deltaX / (float)deltaT;
+	printf("Velocity: %f\n\r", velocity);
 
 	float timeToPickup = distanceToArmCenter/velocity;
 
 	pickupInfo info;
 	info.time = timeToPickup;
-
+	info.x = 0.0;
+	info.y = - 200 - ((startIRVal_1 + startIRVal_2) / 2.0 - 6) *10;
 	return info;
 
 }
@@ -111,13 +181,30 @@ pickupInfo calcPickupInfo()
 int main(void)
 {
 	setup();
-//	home();
-//	setServo(0, 0);
-//	pickupInfo info = calcPickupInfo();
-//	printf("Time to interception: %f\n\r", info.time);
+	home();
+	openGripper(1);
+	gotoXY(60, -200);
+	setServo(0, 0);
+	pickupInfo info = calcPickupInfo();
+	_delay_ms((int)info.time);
+	printf("%d, %d", (int)info.x, (int)info.y);
+	gotoXY(0, (int)info.y);
+	while (!IN_RANGE(upperLinkActual, upperLinkSetpoint - 1, upperLinkSetpoint + 1) &&
+			!IN_RANGE(lowerLinkActual, lowerLinkSetpoint - 1, lowerLinkSetpoint + 1))
+		;
+	closeGripper(1);
+	_delay_ms(1000);
+	gotoXY(60, -200);
+//	while(1)
+//	{
+//		int pos[2];
+//		calcXY(pos);
+//		printf("%d \t %d\n\r", pos[0], pos[1]);
+//		_delay_ms(10);
+////	printf("IR Values: %d, %d\n\r", IRDist(6), IRDist(7));
+//	}
 	while(1)
-	{
-		printf("IR Values: %d, %d\n\r", IRDist(6), IRDist(7));
-	}
+		;
+
 
 }
